3730ca147da91c8488a73e02592a9021771182fb
developerMasha <mashasoori@gmail.com>
2018-12-04 19:09:13 +0800

    test index.js

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ app.get("/",function(request,response){
 	response.send('<h1>This is my web app</h1>');
 });
 
+//show default page on 127.0.0.1:{listening port}/something
+app.get("/something",function(request,response){
+	response.send('<h1>This is something</h1>');
+});
+
+app.get("/yourname",function(request,response){
+	response.send('<h1>This is yourname</h1>');
+});
+
 //Start the express server to listen to a port in the server
     var listener = app.listen(process.env.PORT,
         process.env.IP,
