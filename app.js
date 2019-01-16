83d3be2bef151f96a0021ee6fc2e2944a879aa45
developerMasha <mashasoori@gmail.com>
2019-01-16 22:42:12 +0800

    weather test

--- app.js
+++ app.js
@@ -49,6 +49,29 @@ app.post('/fulfillment', functions.https.onRequest((request, response) => {
         agent.add(`The weather is good!`);
     }
 
+    // Weather Intent
+    app.post('/ai', (req, res) => {
+        if (req.body.result.action === 'weather') {
+          let city = req.body.result.parameters['JapanCity'];
+          let restUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID='+WEATHER_API_KEY+'&q='+city;
+      
+          request.get(restUrl, (err, response, body) => {
+            if (!err && response.statusCode == 200) {
+              let json = JSON.parse(body);
+              let msg = json.weather[0].description + ' and the temperature is ' + json.main.temp + ' ℉';
+              return res.json({
+                speech: msg,
+                displayText: msg,
+                source: 'weather'});
+            } else {
+              return res.status(400).json({
+                status: {
+                  code: 400,
+                  errorType: 'I failed to look up the city name.'}});
+            }})
+        }
+    //end of weather intent
+
     // Run the proper function handler based on the matched Dialogflow intent name
     let intentMap = new Map();
     intentMap.set('Default Welcome Intent', welcome);
