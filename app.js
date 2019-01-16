12423bd0745447ab05df5c3b3ac6f4b73881897e
developerMasha <mashasoori@gmail.com>
2019-01-16 23:40:25 +0800

    weather13

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.post('/fulfillment', functions.https.onRequest((request, response) => {
     // Weather Intent
     function tellweatherfunction(agent) {
        var city = request.queryResult.parameters.JapanCity
-        agent.add(`The weather is good.`+city+'!');
+        agent.add('The weather is good!');
         //end of weather intent
     
     }
