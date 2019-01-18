cfc7d81f0ce0b8a06de1b02e708d575c59531fd8
developerMasha <mashasoori@gmail.com>
2019-01-18 23:42:04 +0800

    weather22

--- app.js
+++ app.js
@@ -54,15 +54,12 @@ app.post('/fulfillment', functions.https.onRequest((request, response) => {
         let city = 'portland';
         let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
-        request(url, function (err, response, body) {
-            let weather = JSON.parse(body)
-            let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        let weather = JSON.parse(body)
+        let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
         
-     
         agent.add(url);
         agent.add(message);
         agent.add(agent.parameters['JapanCity']+' weather in '+agent.parameters['Seasons']);
-    });
         //end of weather intent
     
     }
