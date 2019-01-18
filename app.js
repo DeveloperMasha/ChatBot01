1cf802a2ce2b4a85f088385f41f3180c37ef0f62
developerMasha <mashasoori@gmail.com>
2019-01-18 23:39:48 +0800

    weather21

--- app.js
+++ app.js
@@ -55,17 +55,14 @@ app.post('/fulfillment', functions.https.onRequest((request, response) => {
         let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
         request(url, function (err, response, body) {
-        if(err){
-            console.log('error:', error);
-        } else {
             let weather = JSON.parse(body)
             let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        }
-     });
+        
+     
         agent.add(url);
         agent.add(message);
         agent.add(agent.parameters['JapanCity']+' weather in '+agent.parameters['Seasons']);
-    
+    });
         //end of weather intent
     
     }
