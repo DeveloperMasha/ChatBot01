af9d8c54c3ebd7718ef58c99c9cb62d9dd052c22
developerMasha <mashasoori@gmail.com>
2019-01-19 12:17:35 +0800

    wetahare

--- app.js
+++ app.js
@@ -49,17 +49,20 @@ app.post('/fulfillment', functions.https.onRequest((request, response) => {
     // Weather Intent
     function tellweatherfunction(agent) {
     //    let request = require('request');
-
-        let apiKey = 'f94fb06603ef464c16a935d57b3e2eb1';
-        let city = 'portland';
-        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
-
+    //    let apiKey = 'f94fb06603ef464c16a935d57b3e2eb1';
+    //    let city = 'portland';
+    //     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
     //    let weather = JSON.parse(body);
-    //    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-    
-        agent.add(url);
+    //    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;  
+    //    agent.add(url);
     //    agent.add(weather);
-        agent.add(agent.parameters['JapanCity']+' weather in '+agent.parameters['Seasons']);
+    //    agent.add(agent.parameters['JapanCity']+' weather in '+agent.parameters['Seasons']);
+        if (agent.parameters['Seasons'] == "Winter") { 
+            agent.add(agent.parameters['JapanCity']+' weather in '+agent.parameters['Seasons']+'is generally cold');
+        }
+        else if (agent.parameters['Seasons'] == "Spring"){
+            agent.add(agent.parameters['JapanCity']+' weather in '+agent.parameters['Seasons']+'is generally cooling');
+        }
         //end of weather intent
     
     }
