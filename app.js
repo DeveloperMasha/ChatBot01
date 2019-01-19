73f27e59f7a88dc5f02b0abd4614df58c8039d45
developerMasha <mashasoori@gmail.com>
2019-01-19 11:56:52 +0800

    weatehr1

--- app.js
+++ app.js
@@ -48,17 +48,17 @@ app.post('/fulfillment', functions.https.onRequest((request, response) => {
 
     // Weather Intent
     function tellweatherfunction(agent) {
-        let request = require('request');
+    //    let request = require('request');
 
-        let apiKey = 'f94fb06603ef464c16a935d57b3e2eb1';
-        let city = 'portland';
+    //  let apiKey = 'f94fb06603ef464c16a935d57b3e2eb1';
+    //  let city = 'portland';
         let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
-        let weather = JSON.parse(body)
-        let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    //    let weather = JSON.parse(body)
+    //    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
         
         agent.add(url);
-        agent.add(message);
+    //    agent.add(message);
         agent.add(agent.parameters['JapanCity']+' weather in '+agent.parameters['Seasons']);
         //end of weather intent
     
