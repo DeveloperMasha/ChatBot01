8d21f1502e0459313f422d2fb0f3bc493e43cc81
developerMasha <mashasoori@gmail.com>
2019-01-19 12:03:30 +0800

    weather4

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.post('/fulfillment', functions.https.onRequest((request, response) => {
 
         let apiKey = 'f94fb06603ef464c16a935d57b3e2eb1';
         let city = 'portland';
-        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
     //    let weather = JSON.parse(body)
     //    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
