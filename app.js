dd109028a81bfa690bbe32c574e505c23e47fe61
developerMasha <mashasoori@gmail.com>
2019-01-19 12:57:56 +0800

    wathertest

--- app.js
+++ app.js
@@ -58,15 +58,19 @@ app.post('/fulfillment', functions.https.onRequest((request, response) => {
     //    agent.add(weather);
     //    agent.add(agent.parameters['JapanCity']+' weather in '+agent.parameters['Seasons']);
 
-        if (agent.parameters['JapanCity'] != "Hokkaido" && agent.parameters['Seasons'] == "Winter") { 
-            agent.add(agent.parameters['JapanCity']+' weather in '+agent.parameters['Seasons']+' is generally cold');
+        if (agent.parameters['Seasons'] == "Winter") { 
+            if (agent.parameters['JapanCity'] == "Hokkaido"){
+                agent.add("It is the coldest month in Hokkaido. No matter where you are in this vast prefecture, you'll feel really cold.");
+                agent.add("Average temperatures lie around -4°C (25°F). When it's cold, it gets as low as -6°C (21°F), and it's around -1°C (30°F) even when it's not so cold");
+            }
+            agent.add(agent.parameters['JapanCity']+' weather in '+agent.parameters['Seasons']+' is generally mild');
         }
         else if (agent.parameters['Seasons'] == "Spring"){
             agent.add(agent.parameters['JapanCity']+' weather in '+agent.parameters['Seasons']+' is generally cooling');
         }
         else if (agent.parameters['Seasons'] == "Summer"){
             if (agent.parameters['JapanCity']=="Hokkaido") {
-                agent.add('Summer in Hokkaido is coolder and the temperature in Hokkaido is around 20 ℃. ');
+                agent.add('Summer in Hokkaido is cooler and the temperature in Hokkaido is around 20 ℃. ');
             }
             else {
                 agent.add(agent.parameters['JapanCity']+' weather in '+agent.parameters['Seasons']+' is generally hot');
